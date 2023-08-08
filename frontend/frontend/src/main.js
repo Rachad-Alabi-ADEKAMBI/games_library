886021d4591767168674d8a4d9b38dd7004f7126
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2023-08-08 19:57:55 +0100

    any sin

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
-
-import BootstrapVue from 'bootstrap-vue';
-
-
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 createApp(App).use(store).use(router).mount('#app')
