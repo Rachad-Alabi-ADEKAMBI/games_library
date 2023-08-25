0a725e8c98c44a0d822e6d29cc67e52a21888d2e
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2023-08-25 21:19:05 +0100

    any sin

--- backend/main.py
+++ backend/main.py
@@ -53,9 +53,7 @@ def all_games():
     return jsonify(response_object)
 
 
-#delete
-@app.route('/deleteGame', methods=['POST'])
-def delete():
+def insert():
     response_object = {'status': 'success'}
 
     if request.method == 'POST':
@@ -65,42 +63,9 @@ def delete():
             'genre': post_data.get('genre'),
             'played': post_data.get('played')
         })
-        response_object['message'] = 'Game deleted!'
+        response_object['message'] = 'Game added!'
     else:
         response_object['games'] = GAMES
-
-    return jsonify(response_object)
-
-
-#update
-@app.route('/updateGame', methods=['POST'])
-def delete():
-    response_object = {'status': 'success'}
-
-    if request.method == 'POST':
-        post_data = request.get_json()
-        GAMES.append({
-            'title': post_data.get('title'),
-            'genre': post_data.get('genre'),
-            'played': post_data.get('played')
-        })
-        response_object['message'] = 'Game updated!'
-    else:
-        response_object['games'] = GAMES
-
-    return jsonify(response_object)
-
-#add
-@app.route('/add', methods = ['POST'])
-def add():
-    if request.method == 'POST':
-        new_game = {
-             'name' = request.form['name'],
-             'genre' = request.form['genre'],
-            'played'= request.form['played', False ]  # Default to False if not provided
-        }
-        GAMES.append(new_game)
-        response_object = {'status': 'success', 'message': 'Game Added !'}
 
     return jsonify(response_object)
 
